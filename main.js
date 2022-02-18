16e3de32870dbdc727e34ef4147dbcda99e0f721
Jennifer Udensi <okosisijenifer@gmail.com>
2022-02-18 04:37:00 -0300

    responsiveness

---
+++ main.js
@@ -0,0 +1,14 @@
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper('.food-menu-box', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
